04988ca5c269b32f256034a6b181468ad1cb9587
Ishaan11042005 <ishaanmittal1104.com>
2026-06-11 15:08:22 +0530

    backened

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,6 @@ import {Route, Routes} from 'react-router-dom'
 import LoginPage from './pages/LoginPage.jsx'
 import HomePage from './pages/HomePage.jsx'
 import ProfilePage from './pages/ProfilePage.jsx'
-/*hello*/
 const App = () => {
   return (
     <div className="min-h-screen">
