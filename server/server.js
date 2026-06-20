8fd20295ed135d4539c114690221e72d55ddd41f
Ishaan11042005 <ishaanmittal1104.com>
2026-06-20 14:45:46 +0530

    on vercel

--- server/server.js
+++ server/server.js
@@ -48,11 +48,15 @@ app.use("/api/status", (req, res) => {
 app.use("/api/auth", userRouter);
 app.use("/api/messages", messageRouter);
 
-const PORT = process.env.PORT || 5000;
+
 
 // Connect to the database
 await connectDB();
 
-server.listen(PORT, () => {
+if(process.env.NODE_ENV!=="production"){
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+}
+export default server;
